f1dd51dc143c0976755be7fb6f84b6ded5cd8497
Ayala Shachar <shachar.ayala@gmail.com>
2016-12-14 17:06:44 +0200

    Allow PactGroup.wait to be eager (polling all pacts)

--- pact/group.py
+++ pact/group.py
@@ -1,4 +1,3 @@
-import collections
 import itertools
 
 from .base import PactBase
@@ -6,11 +5,12 @@ from .base import PactBase
 
 class PactGroup(PactBase):
 
-    def __init__(self, pacts=None):
+    def __init__(self, pacts=None, lazy=True):
         if pacts is None:
-            pacts = ()
-        self._pacts = collections.deque(pacts)
+            pacts = []
+        self._pacts = pacts[:]
         self._finished_pacts = []
+        self._is_lazy = lazy
         super(PactGroup, self).__init__()
 
     def __iadd__(self, other):
@@ -26,20 +26,25 @@ class PactGroup(PactBase):
                 raise NotImplementedError('Absorbing groups is not supported') # pragma: no cover
         self._pacts.append(pact)
         if absorb:
+            # pylint: disable=protected-access
             while pact._then:
                 # then might throw, so we attempt it first
                 self.then(pact._then[0])
                 pact._then.pop(0)
 
     def _is_finished(self):
-        while self._pacts:
-            pact = self._pacts[0]
+        has_finished = True
+        indexes_to_remove = []
+        for index, pact in enumerate(self._pacts):
             if pact.poll():
-                self._finished_pacts.append(self._pacts.popleft())
+                indexes_to_remove.insert(0, index)
             else:
-                return False
-        # no more pacts
-        return True
+                has_finished = False
+                if self._is_lazy:
+                    break
+        for index in indexes_to_remove:
+            self._finished_pacts.append(self._pacts.pop(index))
+        return has_finished
 
     def __repr__(self):
         return repr(list(self._pacts))
